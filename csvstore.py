5ff9980328c4b0f7684c8d5c383eda17f4c11274
danfaizer <dbeneyto@gmail.com>
2013-05-25 17:43:28 +0200

    Added station persistence in CSV store method

--- csvstore.py
+++ csvstore.py
@@ -6,8 +6,9 @@
 
 import datetime
 import ConfigParser
-import csv
+import shutil
 import difflib
+import os
 
 class Csvstore:
     def __init__(self):
@@ -36,25 +37,40 @@ class Csvstore:
 
         ## TODO - CORRECT STATION STORE ##
         try:
-            with open(self.csvpath+'/'+bikesystem+'_station.csv.'+datetime.datetime.now().strftime("%Y%m%d-%H"),'w') as fd:
-                # Check if there is any new station
-                fd.write(stations_detail)
-                fd.close()
-                new_fd = open(self.csvpath+'/'+bikesystem+'_station.csv.'+datetime.datetime.now().strftime("%Y%m%d-%H"),'r')
-                old_fd = open(self.csvpath+'/'+bikesystem+'_station.csv','r')
-                print difflib.Differ(new_fd, old_fd)
+            # We firstly check if the file X_station.csv already exists
+            with open(self.csvpath+'/'+bikesystem+'_station.csv','r') as fd:
+                # If the file exists, we compare files to check if there is any new station
+                new_fd = open(self.csvpath+'/'+bikesystem+'_station.csv.'+datetime.datetime.now().strftime("%Y%m%d-%H"),'w')
+                new_fd.write(stations_detail)
+                new_fd.close()
                 
+                # Iterate through both  reader1 and reader2, compare common row, and append matching column data to test.txt in its matching column
+                new = open(self.csvpath+'/'+bikesystem+'_station.csv.'+datetime.datetime.now().strftime("%Y%m%d-%H"),'r')
+                old = open(self.csvpath+'/'+bikesystem+'_station.csv','r')
                 
+                diff = difflib.ndiff(new.readlines(), old.readlines())
+                delta = ''.join(x[2:] for x in diff if x.startswith('- '))
+                
+                old.close()
+                new_fd.close()
+
+                
+                if delta:
+                    # If new stations are found, a new station.csv file is created and moved old one to keep historic data
+                    shutil.move(self.csvpath+'/'+bikesystem+'_station.csv', self.csvpath+'/'+bikesystem+'_station.csv.'+datetime.datetime.now().strftime("%Y%m%d-%H%M"))
+                    shutil.move(self.csvpath+'/'+bikesystem+'_station.csv.'+datetime.datetime.now().strftime("%Y%m%d-%H"),self.csvpath+'/'+bikesystem+'_station.csv')
+                    print "New station added: \r\n"+delta
+                else:
+                    os.remove(self.csvpath+'/'+bikesystem+'_station.csv.'+datetime.datetime.now().strftime("%Y%m%d-%H"))
+
         except IOError:
-            print "Arrg"
-            fd = open(self.csvpath+'/'+bikesystem+'_station.csv','w')
-            fd.write(stations_detail)
-            fd.close()
-#        # Store stations and check for new ones
-#        try:
-#            with open(self.csvpath+'/'+bikesystem+'_station.csv') as fd:
-#                fd.write(stations_detail)
-#                fd.close
-#        except IOError:
-#            print 'Oh dear.'
+            try:
+                print "INFO: File station.csv created"
+                # If the file doesn't exist we try to write is straight ahead
+                with open(self.csvpath+'/'+bikesystem+'_station.csv','w') as fd:
+                    fd.write(stations_detail)
+                    fd.close()
+            except IOError:
+                print "ERROR: Wrong file permissions or path "+self.csvpath+" doesn't exist"
+                
         
